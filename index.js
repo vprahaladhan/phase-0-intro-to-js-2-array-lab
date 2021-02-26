c602a4eebb4c75e9d14c20e8871365d2c7ef7941
prahal <vprahaladhan@gmail.com>
2021-02-26 11:46:01 +1100

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = newCat => cats.push(newCat);
+
+const destructivelyPrependCat = newCat => cats.unshift(newCat);
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+const appendCat = newCat => [...cats, newCat];
+
+const prependCat = newCat => [newCat, ...cats];
+
+const removeLastCat = () => cats.slice(0, cats.length - 1);
+
+const removeFirstCat = () => cats.slice(1);
